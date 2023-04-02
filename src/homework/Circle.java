89a2b32393fab317ba37fd9747b92df7c8e99f06
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-02 14:23:55 +0200

    Add Circle

---
+++ src/homework/Circle.java
@@ -0,0 +1,13 @@
+package homework;
+
+import java.io.BufferedReader;
+
+// Получите от пользователя радиус окружности.
+// По введённым координатам точки определите, попадает ли она внутрь, на границу или
+// вне окружности с указанным радиусом и центром в точке O (0, 0).
+public class Circle {
+  public static void main(String[] args) {
+
+
+  }
+}
