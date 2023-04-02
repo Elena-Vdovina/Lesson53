ce3bbe26afca143183708c0bce50f019650b5a20
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-02 13:25:50 +0200

    Initial commit

---
+++ src/homework/Quarter.java
@@ -0,0 +1,9 @@
+package homework;
+
+// По введённым координатам точки определите, в какой координатной четверти
+// (или на какой оси) она находится.
+public class Quarter {
+  public static void main(String[] args) {
+
+  }
+}
