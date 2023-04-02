dadf1039ced3d3e9b0109912f15e2c84ffc98127
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-04-02 14:47:52 +0200

    Add Circle.check

--- src/homework/Circle.java
+++ src/homework/Circle.java
@@ -1,13 +1,32 @@
 package homework;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // Получите от пользователя радиус окружности.
 // По введённым координатам точки определите, попадает ли она внутрь, на границу или
 // вне окружности с указанным радиусом и центром в точке O (0, 0).
 public class Circle {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int r = Integer.parseInt(br.readLine());
+    Point point = Point.read(br);
+    check(r, point);
+  }
 
-
+  public static void check(int r, Point point) {
+    int x = point.getX();
+    int y = point.getY();
+    double r1 = Math.sqrt(x * x + y * y);
+    if (r1 < (double) r) {
+      System.out.println("Точка " + point.toString() + " лежит внутри окружности радиусом " + r);
+    }
+    if (r1 == (double) r) {
+      System.out.println("Точка " + point.toString() + " лежит на окружности радиусом " + r);
+    }
+    if (r1 > (double) r) {
+      System.out.println("Точка " + point.toString() + " лежит вне окружности радиусом " + r);
+    }
   }
 }
